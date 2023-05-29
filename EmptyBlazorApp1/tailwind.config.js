191cf2cd91f536ad64c0e1c35b20318a8ad3ca9c
ignacio-magno <lasterriens@gmail.com>
2023-05-29 19:33:17 -0400

    archivos faltantes-

---
+++ EmptyBlazorApp1/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        '!**/{bin,obj,node_modules}/**',
+        '**/*.{razor,html}',
+    ],
+    theme: {
+        fontSize: {
+            sm: '1rem',
+            base: '1rem',
+            xl: '1.25rem',
+            '2xl': '1.563rem',
+            '3xl': '1.953rem',
+            '4xl': '2.441rem',
+            '5xl': '3.052rem',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
